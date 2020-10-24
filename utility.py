adaa742d0c1c9b81c9ae5db244ab06c3c557800a
cchandler3 <cchandler3@gatech.edu>
2020-10-24 00:19:41 -0400

    utility: added random forest for comparison. Now printing metrics. Added learning rate decay schedule to neural net training.

--- utility.py
+++ utility.py
@@ -3,6 +3,8 @@ import numpy as np
 from bokeh.plotting import figure, output_file, show
 import nn_model
 import torch
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 
 np.random.seed(1)
 
@@ -72,6 +74,10 @@ def create_two_feature_plot(df, feat1, feat2, feat1_range=None, add_y_jitter=Fal
     show(p)
 
 
+def get_test_data():
+    return pd.read_csv('data/test.csv', converters=TITANIC_DATA_CONVERTERS)
+
+
 def get_training_data(convert=False):
     if convert:
         return pd.read_csv('data/train.csv', converters=TITANIC_DATA_CONVERTERS)
@@ -79,12 +85,22 @@ def get_training_data(convert=False):
         return pd.read_csv('data/train.csv')
 
 
+def print_metrics(predictions, labels, model_name):
+    performances = confusion_matrix(y_true=labels, y_pred=predictions)
+    precision = performances[1][1] / (performances[1][1] + performances[0][1])
+    recall = performances[1][1] / (performances[1][1] + performances[1][0])
+    accuracy = (performances[1][1] + performances[0][0]) / len(labels)
+
+    print(f'{model_name}\n\tPrecision: {precision}, Recall: {recall}, Accuracy: {accuracy}')
+
+
 def train_model(features, labels):
     model = nn_model.TitanicPredictor(len(features[0]), 1)
     loss_fn = torch.nn.MSELoss()
-    optimzer = torch.optim.Adam(model.parameters(), lr=0.01)
+    optimzer = torch.optim.Adam(model.parameters(), lr=0.1)
+    scheduler = torch.optim.lr_scheduler.MultiplicativeLR(optimzer, lr_lambda=lambda epoch: .98)
 
-    EPOCHS = 1000
+    EPOCHS = 30000
     loss_list = []
 
     for i in range(EPOCHS):
@@ -93,22 +109,41 @@ def train_model(features, labels):
         loss = loss_fn(predictions, labels)
         loss_list.append(loss)
 
-        if epoch_num % 25 == 0:
+        if epoch_num % 100 == 0:
             print(f'Epoch: {epoch_num} Loss: {loss}')
 
         optimzer.zero_grad()
         loss.backward()
         optimzer.step()
+        if epoch_num % 100 == 0:
+            scheduler.step()
+            if epoch_num % 500 == 0:
+                print(f'\tlr is {scheduler.state_dict()["_last_lr"]}')
+
+    return model
 
 # df = get_training_data()
 # create_two_feature_plot(df, 'Sex', 'Age', feat1_range=['male', 'female'])
 # create_two_feature_plot(df, 'Sex', 'Pclass', feat1_range=['male', 'female'], add_y_jitter=True)
 
 
-df = get_training_data(convert=True)
+train_df = get_training_data(convert=True)
 features_for_training = ['Pclass', 'Sex', 'SibSp', 'Parch', 'Fare']
-train_features = torch.as_tensor(df[features_for_training].values.astype(np.float), dtype=torch.float)
-train_labels = torch.as_tensor(df['Survived'].values.astype(np.float), dtype=torch.float).unsqueeze(1)
-train_model(train_features, train_labels)
+train_features = torch.as_tensor(train_df[features_for_training].values.astype(np.float), dtype=torch.float)
+train_labels = torch.as_tensor(train_df['Survived'].values.astype(np.float), dtype=torch.float).unsqueeze(1).detach()
+
+nn_model = train_model(train_features, train_labels)
+training_predictions = nn_model.forward(train_features.detach()).detach().round()
+print_metrics(predictions=training_predictions, labels=train_labels, model_name='Neural Network')
+
+forest_model = RandomForestClassifier(max_depth=None, n_estimators=500, random_state=1)
+forest_model.fit(train_df[features_for_training].values, train_df['Survived'].values)
+training_predictions = forest_model.predict(train_df[features_for_training].values)
+print_metrics(predictions=training_predictions, labels=train_labels, model_name='Random Forest')
+
+# Generate predictions to submit
+# test_df = get_test_data()
+# test_features = torch.as_tensor(test_df[features_for_training].values.astype(np.float), dtype=torch.float)
+# predictions = model.forward(test_features)
 
 
